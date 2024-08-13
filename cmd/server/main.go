5aea9390f84faec94877e7309cb31c621a0d1626
DaniyarYermagambetov <daniyaryermagambetov09@gmail.com>
2024-08-13 22:29:14 +0500

    create template for first sprint

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,3 +1,31 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"net/http"
+)
+
+type MemStorage struct {
+	storage map[string][]byte
+}
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", Post)
+
+	var gauge float64
+	var counter int64
+	fmt.Println("Listening on port 8080", gauge, counter)
+	http.ListenAndServe(":8080", mux)
+
+}
+
+func Post(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		http.Error(w, "Unresolved method", http.StatusMethodNotAllowed)
+		return
+	} else {
+		w.WriteHeader(http.StatusCreated)
+	}
+
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/DaniyarYermagambetov/learngo
+
+go 1.22.6
